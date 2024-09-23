e80b726b99231c3fbfa7083fe96fbf2d480b97d3
sweekarJ <joshi.sweekar@gmail.com>
2024-09-23 23:17:15 +0100

    pom fixes and stepdefs
    pom fixes and stepdefs

---
+++ src/test/java/com/example/demo/steps/MyStepDefs.java
@@ -0,0 +1,28 @@
+package com.example.demo.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyStepDefs {
+    private static final Logger logger = LoggerFactory.getLogger(MyStepDefs.class);
+
+    @Given("I have a precondition")
+    public void i_have_a_precondition() {
+        // Your code here
+    }
+
+    @When("I perform a GET request to the API")
+    public void i_perform_an_action() {
+        // Your code here
+        logger.info("running blank step");
+    }
+
+    @Then("the response status should be 200")
+    public void i_expect_a_result() {
+        // Your code here
+        logger.info("running blank step");
+    }
+}
